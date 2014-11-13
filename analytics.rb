5124c274eef0c920b94fc346f757a5c89a7dd3bf
Lavir the Whiolet <Lavir.th.Whiolet@gmail.com>
2014-11-13 13:56:33 +0500

    ...

--- analytics.rb
+++ analytics.rb
@@ -5,12 +5,14 @@ require 'yaml'
 require 'mathn'
 require 'set'
 
+#### COMMON LIBRARY ####
+
 # Comment out/uncomment content of specific method to disable/enable
 # particular messages.
 module Logging
 
   def debug_info(msg)
-#     STDERR.puts "debug: #{msg}"
+    STDERR.puts "debug: #{msg}"
   end
 
   def info(msg)
@@ -89,15 +91,16 @@ class Numeric
   
 end
 
+#### BUSINESS LOGIC ####
+
 def mapreduce
   CSV.foreach(ARGV[0]) do |row|
-    row_id = row[0].to_i
-    msg = row[1]
-    nick = row[2]
-    yield row_id, msg, nick
+    yield(row_id = row[0].to_i, msg = row[1], nick = row[2])
   end
 end
 
+# Pass 1: Collect nicks an aliases.
+#   Note. Each person has one nick. Each nick may have zero or more aliases.
 known_aliases = YAML.load <<DATA
   # alias: nick
   Цивет под снегом: Цивет
@@ -128,10 +131,21 @@ known_aliases = YAML.load <<DATA
   Wormhole: Марислава
   z-b: zxc
   sw: mynameiswinner
-  
+  tallman: /s/tallman
+  лолита-ультрас: лолита
+  lolita: лолита
+  lola: лолита
+  лола: лолита
 DATA
-aliases = known_aliases
+aliases = known_aliases # .dup
 nicks = Set.new(aliases.values)
+orphan_aliases = Set.new
+add_orphan_alias = lambda do |alias_|
+  if not orphan_aliases.include? alias_
+    debug_info %(orphan alias: #{alias_})
+    orphan_aliases.add alias_
+  end
+end
 mapreduce do |row_id, msg, sender|
   if sender == ""
     if /^coding@conference.jabber.ru\/(.*) \-\> (.*)$/ === msg then
@@ -158,37 +172,44 @@ mapreduce do |row_id, msg, sender|
         end
       end
     elsif /^coding@conference.jabber.ru\/(.*) has joined$/ === msg then
-      nick = $1
-      debug_info %(new nick: #{nick})
-      nicks.add nick
+      # This may be a new person or an old person with a different alias.
+      add_orphan_alias.($1)
     end
+  elsif "<coding@conference.jabber.ru" === msg then
+    # ignore
+  else
+    # We don't know if it is a new person or an old person with a different
+    # alias.
+    add_orphan_alias.(sender)
   end
 end
-nicks_intersection = (aliases.keys & aliases.values)
-if not nicks_intersection.empty? then
+if not (intersection = (aliases.keys & aliases.values)).empty? then
   info %(aliases:)
   aliases.each { |alias_, nick| info %(  #{alias_}: #{nick}) }
   error %(aliases map is not normal; following nicks are aliases too: #{nicks_intersection.join(", ")})
 end
+additional_nicks = orphan_aliases.subtract(aliases.keys)
+nicks = nicks.union(additional_nicks)
 info %(nicks:)
 nicks.each { |nick| info "  #{nick}" }
 
-# Map-reduce: Collect analytics
+# Pass 2: Collect analytics
 results = Hash.new do |hash, nick|
   hash[nick] = Hash.new { |hash, key| hash[key] = 0 }
 end
 max_row_id = 0
 row_index = 0
 mapreduce do |row_id, msg, nick|
-  # Take n first elements.
-#  break if row_index >= 1000
+#   # Take n first elements.
+#   break if row_index >= 1000
   # Filter out unneeded elements.
-  next if nick == ""
+  next if nick == "" or nick == "<coding@conference.jabber.ru"
   # Process!
   nick = aliases[nick] || nicks[nick] || error("unknown nick #{nick}")
   max_row_id = [row_id, max_row_id].max
   results[nick]["messages"] += 1
   results[nick]["messages size"] += msg.size
+  # 
   row_index += 1
 end
 total_messages = results.map { |_, data| data["messages"] }.sum
